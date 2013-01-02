d9e81bebc2dc85ca45f2350b3d94a385d4c60e80
Simon Whitaker <simon@goosoftware.co.uk>
2013-01-02 22:38:48 +0000

    Remove redundant call to sranddev

--- GSProgressViewDemo/GSProgressViewDemo/GSViewController.m
+++ GSProgressViewDemo/GSProgressViewDemo/GSViewController.m
@@ -18,7 +18,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    sranddev();
 
     float x = 1.0;
     for (GSProgressView *progressView in self.progressViews)
